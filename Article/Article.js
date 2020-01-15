419c2014219a9e57c727da1dee5d48e9628f2b60
elontesla271 <ou40a9rtbp@gmail.com>
2020-01-15 12:43:19 -0800

    made component creator function

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,35 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+function createArticle(data) {
+  let container = document.createElement('div');
+  container.classList.toggle('article');
+
+  let title = document.createElement('h2');
+  title.textContent = data.title;
+
+  let date = document.createElement('p');
+  data.classList.toggle('date');
+  date.textContent = data.date;
+
+  let firstP = document.createElement('p');
+  firstP.textContent = data.firstParagraph;
+  let secondP = document.createElement('p');
+  secondP.textContent = data.secondParagraph;
+  let thirdP = document.createElement('p');
+  thirdP.textContent = data.thirdParagraph;
+
+  let button = document.createElement('span');
+  button.classList.toggle('expandButton');
+  button.addEventListener('click', () => container.toggle('article-open'));
+
+  container.append(title);
+  container.append(date);
+  container.append(firstP);
+  container.append(secondP);
+  container.append(thirdP);
+  container.append(button);
+
+  return container;
+}
